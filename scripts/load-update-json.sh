b95cbf026666e2879e2b36afc8d1096f269e3fba
Karl Jonathan Ward <karl.j.ward@gmail.com>
2017-01-18 16:56:58 +0000

    Script to push update json to feed api

--- scripts/load-update-json.sh
+++ scripts/load-update-json.sh
@@ -4,4 +4,4 @@ docker exec cayennedevelop_feeder_1 mkdir -p /app/data/feed-in
 docker exec cayennedevelop_feeder_1 mkdir -p /app/data/feed-failed
 docker exec cayennedevelop_feeder_1 mkdir -p /app/data/feed-processed
 
-docker cp $1 cayennedevelop_feeder_1:/app/data/feed-in/crossref-unixsd-1.body
+docker cp $1 cayennedevelop_feeder_1:/app/data/feed-in/crossref-update-1.body
